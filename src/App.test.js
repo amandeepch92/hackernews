0a421694859db7ec994355ebdaf56b9097355298
ankit59111 <ankitsingh5991111@gmail.com>
2019-03-14 21:06:03 +0530

    added 3 test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Enzyme,{EnzymeAdapter,shallow,mount,ShallowWrapper} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import renderer from 'react-test-renderer';
+import Homepage from "./body/homepage/homepage";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({ adapter: new Adapter() })
+
+describe("check home page component",() => {
+  it("renders homepage with isLoaded false",() => {
+    const  wrapper = shallow(<Homepage/>)
+    expect(wrapper.find("div.main-body").length).toEqual(0);
+  })
+
+
+  it("check homepage  isLoaded value",() => {
+    const  wrapper = shallow(<Homepage/>)
+    let isLoaded = wrapper.state("isLoaded")
+    expect(isLoaded).toEqual(false);
+  })
+
+  it("check homepage with isLoaded True",()=>{
+    const  wrapper = shallow(<Homepage/>)
+    let isLoaded = wrapper.setState({isLoaded:true,alldata:[]})
+    expect(wrapper.find("div.main-body").length).toEqual(1);
+  })
+
+
+})
