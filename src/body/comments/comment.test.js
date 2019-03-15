f1336ae4fa5daa408aa51270a26ac66771c4a22c
ankit59111 <ankitsingh5991111@gmail.com>
2019-03-15 10:04:39 +0530

    added comment test case

---
+++ src/body/comments/comment.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Enzyme,{shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Comments from "./comments";
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe("check home page component",() => {
+    it("renders homepage with isLoaded false",() => {
+        const  wrapper = shallow(<Comments match={{"params":{id:""}}}/>)
+        expect(wrapper.find("div.header").length).toEqual(0);
+    })
+
+
+    it("check homepage  isLoaded value",() => {
+        const  wrapper = shallow(<Comments match={{"params":{id:""}}}/>)
+        let isLoaded = wrapper.state("isLoaded")
+        expect(isLoaded).toEqual(false);
+    })
+
+    it("check homepage with isLoaded True",()=>{
+        const  wrapper = shallow(<Comments match={{"params":{id:""}}}/>)
+         wrapper.setState({isLoaded:true,data:{children:{}}})
+        expect(wrapper.find("div.header").length).toEqual(1);
+    })
+
+
+})
